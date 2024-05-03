6cf0a1057bc20bf725c0ea14a7f2e7fd7bd7b950
Ikigai <alvardsandz@gmail.com>
2024-05-03 21:01:08 +0200

    add log results popularMovies

--- js/movies.js
+++ js/movies.js
@@ -5,6 +5,7 @@ import { shuffle } from './shuffle.js';
 export async function displayPopularMovies() {
   const popularMovies = document.getElementById('popular-movies');
   const { results } = await fetchAPIData('movie/popular');
+console.log('Movies: ', results, popularMovies);
   results.map((movie) => {
     popularMovies.innerHTML += `
     <div class="card">
